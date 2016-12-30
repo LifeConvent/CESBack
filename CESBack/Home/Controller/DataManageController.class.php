ad5b371278e28bfb5dd45f64451452c1bcfc5eaa
lawrance <lawrance@LawrancedeMacBook-Pro.local>
2016-12-30 16:44:29 +0800

    新增getSurveyImageCount问卷结果处理，单选，暂时未完成

--- CESBack/Home/Controller/DataManageController.class.php
+++ CESBack/Home/Controller/DataManageController.class.php
@@ -80,8 +80,65 @@ class DataManageController extends Controller
     public function getSurveyImageCount()
     {
         $survey_id = I('post.s_i');
+        $survey_id = '1480946991';
         $survey = M('survey');
         $condition_survey['survey_id'] = "$survey_id";
         $result = $survey->where($condition_survey)->select();
+        $string = $result[0]['question'];
+        $survey_name = $result[0]['name'];
+        $que_list = M('survey_question');
+        /**
+         * 当前问卷的所有问题
+         */
+        $result_question = $que_list->where('question_id IN (' . $string . ')')->select();
+        $string = explode(',', $string);
+
+//        dump($result_question);
+        /**
+         * 针对每个问题获取答案
+         */
+        $answer = M('');
+        //二维解析结果坐标
+        $one = $two = 0;
+        for ($i = 0; $i < sizeof($result_question); $i++) {
+            $condition_answer = null;
+            $question_id = $result_question[$i]['question_id'];
+            $condition_answer['survey_id'] = "$survey_id";
+            $condition_answer['question_id'] = "$question_id";
+            $result_answer = $answer->table('tb_survey_answer')
+                ->field('content,COUNT(*) AS total')
+                ->where($condition_answer)
+                ->query('SELECT %FIELD% FROM %TABLE% %WHERE% GROUP BY content', true);
+            $result_total = $answer->table('tb_survey_answer')
+                ->field('COUNT(*) AS total')
+                ->where($condition_answer)
+                ->query('SELECT %FIELD% FROM %TABLE% %WHERE% GROUP BY question_id', true);
+            $total = (int)$result_total[0]['total'];
+
+            /**
+             * 解析封装处理结果
+             */
+            $output['name'] = $survey_name;
+            $output['id'] = $survey_id;
+            if ($result_question[$i]['type'] == '1') {//当前问题的类型
+                $output[$one]['name'] = $result_question[$i]['name'];
+                $output[$one]['id'] = $result_question[$i]['question_id'];
+                $content = $result_question[$i]['content'];
+                //解析content转换为数组
+
+                $question_list = null;
+                $question_list = $content;
+                //问题数量的多少不一定与答案查询结果中group的多少相匹配
+                for ($j = 0; $j < sizeof($question_list); $j++) {
+                    $output[$one][$j]['name'] = $question_list[$i]['name'];
+                    $output[$one][$j]['num'] = (float)$result_answer[$i]['total'] / $total;
+                }
+                $one++;
+            }
+
+            dump($total);
+            dump($result_answer);
+            dump($output);
+        }
     }
 }
