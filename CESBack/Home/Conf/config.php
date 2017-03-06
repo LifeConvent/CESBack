c4665d469fdb7bb198ca3b42d20f450a9eb30eb2
lawrance <lawrance@LawrancedeMacBook-Pro.local>
2017-03-06 17:33:50 +0800

    数据库试改

--- CESBack/Home/Conf/config.php
+++ CESBack/Home/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'localhost',
-    'DB_NAME'=>'CES',
-    'DB_USER'=>'root',
-    'DB_PWD'=>'scce317ces/back.',
+    'DB_HOST'=>'tcp:tvusl4wxps.database.chinacloudapi.cn',
+    'DB_NAME'=>'scceA4FoxI8lRc2P',
+    'DB_USER'=>'scce@tvusl4wxps',
+    'DB_PWD'=>'CES317back',
     // 'DB_PORT'=>,
     'DB_PREFIX'=>'tb_',
     'DB_CHARSET'=>'utf8',
